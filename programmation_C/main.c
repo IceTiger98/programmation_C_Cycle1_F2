65db421213c8dda54b07eb3e1f99171d2f9b1a1c
IceTiger98 <56994002+IceTiger98@users.noreply.github.com>
2020-12-02 18:48:39 +0100

    Update main.c

--- programmation_C/main.c
+++ programmation_C/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <window.h>
+#include "bateau.h"
+#include "plateau.h"
+#include "action.h"
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}
+
+
+int main() {
+        int Tplateau[10][10];
+        int bateau[3][2];
+        int tire[2];
+        int essaie=0,
+            touche=0;
+
+        initPlateau(Tplateau);
+        initBateau(bateau);
+
+        printf("\n");
+
+        do{
+            plateau(Tplateau);
+            attaque(tire);
+            essaie++;
+
+            if(coule(tire,bateau)){
+                vise(tire,bateau,essaie);
+                touche++;
+            }
+            else
+                vise(tire,bateau,essaie);
+
+            initPlateau2(tire,bateau,Tplateau);
+
+
+        }while(touche!=3);
+
+        printf("\n\n\n BRAVO, tu viens d'eradiquer tout les bateaux en %d essaies", essaie);
+        plateau(Tplateau);
+    }
+
+
+
